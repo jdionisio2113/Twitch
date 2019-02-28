899eb5fa92e65c4b460bbb96b26854569f91a9b2
Justin Dionisio <justindionisio21@yahoo.com>
2019-02-27 18:05:46 -0800

    Bootstrap Carousel

--- app/components/FeaturedStreams.js
+++ app/components/FeaturedStreams.js
@@ -6,75 +6,135 @@ function Streams(props) {
   var { featured, currentFeaturedStream, onSelect } = props;
   // var mainStream = featured.slice(1);
   console.log(featured);
-
-  // var x = featured.map(function(item) {
-  //   <img src={item.image} />;
-  // });
-
-  // console.log(x);
+  const foo = [1, 2, 3];
   return (
     <div className="featured-streams">
-      {/* <p>{featured.stream.channel.game}</p> */}
-      {/* <div>
-        <img src={currentFeaturedStream.image} />
-
-        <p>{currentFeaturedStream.text}</p>
-      </div> */}
-      {/* <div className="scrollMenu">
-        {featured.map(function(item, index) { */}
-      {/* // console.log(item.stream.channel.game); */}
-
       <div className="game_container">
-        {/* <p>{item.stream.channel.game}</p> */}
-        {/* <a onClick={onSelect.bind(this, item)}>
-                <img className="logo" src={item.stream.preview.small} />
-                
-              </a> */}
         <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={featured.image}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={featured.image}
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={featured.image}
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
+          {/* <Carousel.Item> */}
+          {featured.map(function(item) {
+            return (
+              <Carousel.Item>
+                <div>
+                  <img src={item.image} />
+                  {/* <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.
+                    </p>
+                  </Carousel.Caption> */}
+                </div>
+                <div>
+                  {/* <Carousel.Caption> */}
+                  <h3>First slide label</h3>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                  {/* </Carousel.Caption> */}
+                </div>
+              </Carousel.Item>
+            );
+          })}
+          {/* </Carousel.Item> */}
         </Carousel>
       </div>
-
-      {/* // })} */}
-      {/* // </div> */}
     </div>
   );
+
+  // return (
+  //   <div className="featured-streams">
+  //     {/* <p>{featured.stream.channel.game}</p> */}
+  //     {/* <div>
+  //       <img src={currentFeaturedStream.image} />
+
+  //       <p>{currentFeaturedStream.text}</p>
+  //     </div> */}
+  //     {/* <div className="scrollMenu">
+  //       {featured.map(function(item, index) { */}
+  //     {/* // console.log(item.stream.channel.game); */}
+
+  //     <div className="game_container">
+  //       {/* <p>{item.stream.channel.game}</p> */}
+  //       {/* <a onClick={onSelect.bind(this, item)}>
+  //               <img className="logo" src={item.stream.preview.small} />
+
+  //             </a> */}
+  //       <Carousel>
+  //         <Carousel.Item>
+  //           <h1>{featured.image}</h1>
+  //         </Carousel.Item>
+  //         {/* <Carousel.Item>
+  //           <img
+  //             className="d-block w-100"
+  //             src={featured.image}
+  //             alt="First slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3>First slide label</h3>
+  //             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item>
+
+  //         <Carousel.Item>
+  //           <img
+  //             className="d-block w-100"
+  //             src={featured.image}
+  //             alt="Second slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3>Second slide label</h3>
+  //             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item>
+
+  //         <Carousel.Item>
+  //           <img
+  //             className="d-block w-100"
+  //             src={featured.image}
+  //             alt="Third slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3>Third slide label</h3>
+  //             <p>
+  //               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+  //             </p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item>
+
+  //         <Carousel.Item>
+  //           <img
+  //             className="d-block w-100"
+  //             src={featured.image}
+  //             alt="Fourth slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3>Fourth slide label</h3>
+  //             <p>
+  //               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+  //             </p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item>
+
+  //         <Carousel.Item>
+  //           <img
+  //             className="d-block w-100"
+  //             src={featured.image}
+  //             alt="Fifth slide"
+  //           />
+  //           <Carousel.Caption>
+  //             <h3>Fifth slide label</h3>
+  //             <p>
+  //               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+  //             </p>
+  //           </Carousel.Caption>
+  //         </Carousel.Item> */}
+  //       </Carousel>
+  //     </div>
+
+  //     {/* // })} */}
+  //     {/* // </div> */}
+  //   </div>
+  // );
 }
 
 class FeaturedStreams extends React.Component {
