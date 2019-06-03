c5a206abaf71e986d04f8f84a87eecfa8f299a7d
Justin Dionisio <justindionisio21@yahoo.com>
2019-06-03 00:39:45 -0700

    Created link for top games to redirect to top games category in original Twitch website

--- app/components/TopGames.js
+++ app/components/TopGames.js
@@ -38,10 +38,15 @@ function TopGames(props) {
         {/* <div className="scrollMenu"> */}
         <Slider {...settings}>
           {games.map(function(item, index) {
-            // console.log(item.game.logo.large);
+            var gameName = item.game.name;
             return (
               <div key={index} className="game_container">
-                <img className="logo" src={item.game.box.large} />
+                <a
+                  href={`https://www.twitch.tv/directory/game/${gameName}`}
+                  target="_blank"
+                >
+                  <img className="logo" src={item.game.box.large} />
+                </a>
                 {/* <h1>{item.game.name}</h1> */}
               </div>
             );
